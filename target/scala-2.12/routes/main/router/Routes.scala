9f1161994b8504e46481757c975cfcfcc7e77274
Ryan Thai <r.thai@student.unsw.edu.au>
2017-09-22 10:32:49 +1000

    some changes

--- target/scala-2.12/routes/main/router/Routes.scala
+++ target/scala-2.12/routes/main/router/Routes.scala
@@ -1,7 +1,7 @@
 
 // @GENERATOR:play-routes-compiler
-// @SOURCE:C:/Users/Daniel/ULook/conf/routes
-// @DATE:Fri Sep 22 01:45:24 AEST 2017
+// @SOURCE:/Users/RyanThai/Desktop/ULook/conf/routes
+// @DATE:Fri Sep 22 10:16:49 AEST 2017
 
 package router
 
