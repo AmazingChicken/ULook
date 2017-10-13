4242908d10d9ed5c0704584a9650ab40ade10b35
HaoChen1210 <HaoChen1210@gmail.com>
2017-10-13 14:19:33 +1100

    Add files via upload

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,6 +1,12 @@
 package controllers;
 // Utilise play framework
 import play.mvc.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -40,4 +46,48 @@ public class HomeController extends Controller {
     public Result myOutfits(){
       	return ok(views.html.myOutfits.render());
     }
+
+
+    public Result approach1(String s) {
+      System.out.println(s);
+      if(s.equals(" ")){
+        return ok("Please enter a not empty string");
+      }
+      File file = new File("test.txt");
+      String regEx = s+".*";
+      Pattern pattern = Pattern.compile(regEx);
+    //System.out.println(pattern);
+     // String content = Files.toString(new File("test.txt"));
+      String content = txt2String(file);
+     // return ok(content);
+  //  System.out.println(content);
+      Matcher matcher = pattern.matcher(content);
+      boolean rs = matcher.find();
+    //boolean isMatch = Pattern.matches(pattern, content);
+      if(rs==true){
+        System.out.println(content);
+      }
+      else{
+       // return ok(content);
+      }
+
+    //if (s == 0) return badRequest("Wrong video ID");
+      return ok("1: Display video no. " + s);
+    }
+
+    public static String txt2String(File file){
+        StringBuilder result = new StringBuilder();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String s = null;
+            while((s = br.readLine())!=null){
+                result.append(System.lineSeparator()+s);
+            }
+            br.close();    
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
 }
+
