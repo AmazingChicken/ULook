f9129415577d7ea50f7ab704776c9685cf4ebb70
HaoChen1210 <HaoChen1210@gmail.com>
2017-10-15 15:50:33 +1100

    Add files via upload

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,10 +4,6 @@ import play.mvc.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,6 +11,12 @@ import java.nio.charset.StandardCharsets;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+
+
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -28,10 +30,6 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-
-	
-	
-	
     public Result index() {
         return ok(views.html.index.render());
     }
@@ -44,24 +42,51 @@ public class HomeController extends Controller {
       	return ok(views.html.signUpPage.render());
     }
     public Result myItems(){
-      	return ok(views.html.myItems.render(dummyImage(),dummyOptions(),dummyType()));
+      	return ok(views.html.myItems.render());
     }
     public Result recommend(){
-      	return ok(views.html.recommend.render(dummyOccasion(),dummyOutfit()));
+      	return ok(views.html.recommend.render());
     }
     public Result search(){
-      	return ok(views.html.search.render(dummyImage(),dummyOptions(),dummyType()));
+      	return ok(views.html.search.render());
     }
     public Result getInspired(){
-      	return ok(views.html.getInspired.render(dummyInspiree()));
+      	return ok(views.html.getInspired.render());
     }
     public Result myOutfits(){
-      	return ok(views.html.myOutfits.render(dummyOccasion(),dummyOutfit()));
-    }
-    public Result approach2(String s) {
-        return ok(s);
+      	return ok(views.html.myOutfits.render());
     }
 
+    public Result signUp(String name, String password) throws IOException{
+      FileWriter fw = new FileWriter("app/controllers/1.txt");
+      fw.write("name:"+name+" ");
+      fw.write("password:"+password+"\r\n");
+      fw.close();
+      return ok("Your UserName is :"+ name);
+    }
+    public Result signIn(String name, String password) throws IOException{
+      File file = new File("app/controllers/1.txt");
+      StringBuilder result = new StringBuilder();
+      try{
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String s = null;
+            while((s = br.readLine())!=null){
+                boolean contains1 = s.contains(name);
+                boolean contains2 = s.contains(password);
+                result.append(System.lineSeparator()+s);
+                if(contains1==true && contains2==true){
+                    System.out.println("welcome to Ulook");
+                }
+                
+            }
+            br.close();    
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        String content = result.toString();
+    
+        return ok(views.html.mainPage.render());
+    }
     public Result approach1(String s) {
     //  java.io.File yourFile = new java.io.File("app/controllers/test.txt");
      // java.io.FileReader fr = new java.io.FileReader(yourFile);
@@ -107,67 +132,6 @@ public class HomeController extends Controller {
             e.printStackTrace();
         }
         return result.toString();
-    }
-    //Below are some functions that make dummy data for to generate stuff in the html
-    public Map<String,String> dummyImage(){
-    	Map<String,String> yes = new HashMap<String,String>();
-    	yes.put("roshe","images/roshe.jpg");
-    	yes.put("Nike","images/2.png");
-    	yes.put("Nike 2","images/3.png");
-    	yes.put("Boot","images/boot.jpg");
-    	return yes;
-    }
-    public ArrayList<String> dummyOptions(){
-    	ArrayList<String> yes = new ArrayList<String>();
-    	yes.add("Nike");
-    	yes.add("Adidas");
-    	yes.add("New balance");
-    	yes.add("Puma");
-    	yes.add("Sketchers");
-    	yes.add("Timberland");
-    	
-    	return yes;
-    }
-    public ArrayList<String> dummyType(){
-    	ArrayList<String> yes = new ArrayList<String>();
-    	yes.add("Trainers");
-    	yes.add("Casual");
-    	yes.add("Boots");
-    	yes.add("Sandals");
-    	yes.add("Basketball");
-    	yes.add("Sports");
-    	
-    	return yes;
-    }
-    public ArrayList<String> dummyOccasion(){
-    	ArrayList<String> yes = new ArrayList<String>();
-    	yes.add("Formal");
-    	yes.add("Casual");
-    	yes.add("Sport");
-    	yes.add("Work");
-    	yes.add("Semi-Formal");
-    	yes.add("Beach");
-    	
-    	return yes;
-    }
-    public Map<String,String> dummyInspiree(){
-    	Map<String,String> yes = new HashMap<String,String>();
-    	yes.put("Xenia","images/Xenia.png");
-    	yes.put("andreialbu_","images/andreialbu_.png");
-    	yes.put("blogjuniorestevam","images/blogjuniorestevam.png");
-    	return yes;
-    }
-    public Map<String,Outfit> dummyOutfit(){
-    	Map<String,Outfit> yes = new HashMap<String,Outfit>();
-    	Outfit outfit1 = new Outfit("images/hat.jpg","images/shirt.jpg","images/pants.jpg","images/roshe.jpg");
-    	Outfit outfit2 = new Outfit("images/hat.jpg","images/shirt.jpg","images/pants.jpg","images/boot.jpg");
-    	Outfit outfit3 = new Outfit("images/hat.jpg","images/shirt.jpg","images/pants.jpg","images/chucks.jpg");
-    	Outfit outfit4 = new Outfit("images/hat.jpg","images/shirt.jpg","images/pants.jpg","images/4.png");
-    	yes.put("Outfit1", outfit1);
-    	yes.put("Outfit2", outfit2);
-    	yes.put("Outfit3", outfit3);
-    	yes.put("Outfit4", outfit4);
-    	return yes;
     }
 }
 
