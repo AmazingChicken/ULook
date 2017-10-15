176654759efae1f247c4deeca02fad6f81309c01
HaoChen1210 <HaoChen1210@gmail.com>
2017-10-15 16:08:45 +1100

    Add files via upload

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,9 +4,6 @@ import play.mvc.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -45,19 +42,19 @@ public class HomeController extends Controller {
       	return ok(views.html.signUpPage.render());
     }
     public Result myItems(){
-      	return ok(views.html.myItems.render(dummyImage(),dummyOptions(),dummyType()));
+      	return ok(views.html.myItems.render());
     }
     public Result recommend(){
-      	return ok(views.html.recommend.render(dummyOccasion(),dummyOutfit()));
+      	return ok(views.html.recommend.render());
     }
     public Result search(){
-      	return ok(views.html.search.render(dummyImage(),dummyOptions(),dummyType()));
+      	return ok(views.html.search.render());
     }
     public Result getInspired(){
-      	return ok(views.html.getInspired.render(dummyInspiree()));
+      	return ok(views.html.getInspired.render());
     }
     public Result myOutfits(){
-      	return ok(views.html.myOutfits.render(dummyOccasion(),dummyOutfit()));
+      	return ok(views.html.myOutfits.render());
     }
 
     public Result signUp(String name, String password) throws IOException{
@@ -78,7 +75,7 @@ public class HomeController extends Controller {
                 boolean contains2 = s.contains(password);
                 result.append(System.lineSeparator()+s);
                 if(contains1==true && contains2==true){
-                    System.out.println("welcome to Ulook");
+                    return ok(views.html.mainPage.render());
                 }
                 
             }
@@ -88,7 +85,7 @@ public class HomeController extends Controller {
         }
         String content = result.toString();
     
-        return ok(views.html.mainPage.render());
+        return ok("sorry, the UserName or the Password is Wrong");
     }
     public Result approach1(String s) {
     //  java.io.File yourFile = new java.io.File("app/controllers/test.txt");
@@ -136,66 +133,5 @@ public class HomeController extends Controller {
         }
         return result.toString();
     }
-    
-     //Below are some functions that make dummy data for to generate stuff in the html
-      public Map<String,String> dummyImage(){
-      	Map<String,String> yes = new HashMap<String,String>();
-      	yes.put("roshe","images/roshe.jpg");
-
-      	
-      	return yes;
-      }
-     public ArrayList<String> dummyOccasion(){
-     	ArrayList<String> yes = new ArrayList<String>();
-     	yes.add("Formal");
-     	yes.add("Casual");
-     	yes.add("Sport");
-     	yes.add("Work");
-     	yes.add("Semi-Formal");
-     	yes.add("Beach");
-     	
-     	return yes;
-    }
-     public ArrayList<String> dummyOptions(){
-      	ArrayList<String> yes = new ArrayList<String>();
-      	yes.add("Formal");
-      	yes.add("Casual");
-      	yes.add("Sport");
-      	yes.add("Work");
-      	yes.add("Semi-Formal");
-      	yes.add("Beach");
-      	
-      	return yes;
-     }
-     public ArrayList<String> dummyType(){
-       	ArrayList<String> yes = new ArrayList<String>();
-       	yes.add("Formal");
-       	yes.add("Casual");
-       	yes.add("Sport");
-       	yes.add("Work");
-       	yes.add("Semi-Formal");
-       	yes.add("Beach");
-       	
-       	return yes;
-      }
-     public Map<String,String> dummyInspiree(){
-     	Map<String,String> yes = new HashMap<String,String>();
-     	yes.put("Xenia","images/Xenia.png");
-     	yes.put("andreialbu_","images/andreialbu_.png");
-     	yes.put("blogjuniorestevam","images/blogjuniorestevam.png");
-     	return yes;
-     }
-     public Map<String,Outfit> dummyOutfit(){
-     	Map<String,Outfit> yes = new HashMap<String,Outfit>();
-     	Outfit outfit1 = new Outfit("images/hat.jpg","images/shirt.jpg","images/pants.jpg","images/roshe.jpg");
-     	Outfit outfit2 = new Outfit("images/hat.jpg","images/shirt.jpg","images/pants.jpg","images/boot.jpg");
-     	Outfit outfit3 = new Outfit("images/hat.jpg","images/shirt.jpg","images/pants.jpg","images/chucks.jpg");
-     	Outfit outfit4 = new Outfit("images/hat.jpg","images/shirt.jpg","images/pants.jpg","images/4.png");
-     	yes.put("Outfit1", outfit1);
-     	yes.put("Outfit2", outfit2);
-     	yes.put("Outfit3", outfit3);
-     	yes.put("Outfit4", outfit4);
-     	return yes;
-     } 
 }
 
