660502ea05bdf3717107c8d46fb9e7b66feaf7e7
Junyu Lin(Frank) <396685004@qq.com>
2017-10-17 01:23:47 +1100

    Add files via upload

---
+++ app/controllers/DBUtil.java
@@ -0,0 +1,203 @@
+package controllers;
+
+import java.sql.*;
+import java.util.Properties;
+
+
+public class DBUtil {
+
+    private final String DBURL = "jdbc:postgresql://localhost/playdb";  // CHANGE PORT TO 5432 BY DEFAULT
+    private final String DBUSER = "frank";
+    private final String DBPASSWORD = "";                           // CHANGE PASSWOR
+    private Connection conn = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+
+    public DBUtil() {
+        Properties props = new Properties();
+        props.setProperty("user", DBUSER);
+        props.setProperty("password", DBPASSWORD);
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(DBURL, props);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+
+    public void close() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if(ps != null){
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if(rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    // feel free to test your local database
+    public static void main(String[] args) throws SQLException {
+        String itemName = "aaa";
+        String itemBrand = "bbb";
+        String itemType = "ccc";
+        String itemCategory = "ddd";
+        String itemPrice = "eee";
+        addItem(itemName, itemBrand, itemType, itemCategory, itemPrice);
+
+        try {
+            Item item = getItemBy("name","aaa");
+            System.out.println(item.itemPrice);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //add user into the database (for sign up)
+    public static void addUser(String account, String password){
+        DBUtil dbUtils = new DBUtil();
+        Connection conn = dbUtils.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        System.out.println("asdasdas");
+        try {
+            //ps = conn.prepareStatement("SELECT * FROM \"Login\"");
+            ps = conn.prepareStatement(
+                    "INSERT INTO \"Login\" (\"username\", \"password\") " +
+                            "VALUES (?, ?)");
+            ps.setString(1, account);
+            ps.setString(2, password);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                System.out.println(rs.getString("username"));
+            }
+
+            System.out.println("asdasdas");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            dbUtils.close();
+        }
+    }
+
+    //check whether exists an account with the password
+    public static boolean checkUser(String account,String password) throws SQLException {
+        DBUtil dbUtils = new DBUtil();
+        Connection conn = dbUtils.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+                ps = conn.prepareStatement("SELECT * FROM \"Login\" WHERE \"username\"=?");
+                ps.setString(1, account);
+                rs = ps.executeQuery();
+
+            if (rs.next()) {
+                if(account.equals(rs.getString("userName")) && password.equals(rs.getString("password"))){
+                    return true;
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            dbUtils.close();
+        }
+        return false;
+    }
+
+    //add one item to the database
+    public static void addItem(String itemName, String itemBrand, String itemType, String itemCategory, String itemPrice){
+        DBUtil dbUtils = new DBUtil();
+        Connection conn = dbUtils.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(
+                    "INSERT INTO \"Data\" (\"name\", \"brand\", \"type\", \"category\", \"price\") " +
+                            "VALUES (?, ?, ?, ?, ?)");
+            ps.setString(1, itemName);
+            ps.setString(2, itemBrand);
+            ps.setString(3, itemType);
+            ps.setString(4, itemCategory);
+            ps.setString(5, itemPrice);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                System.out.println(rs.getString("username"));
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            dbUtils.close();
+        }
+    }
+
+    //get item (for search) ; there are five types: name, brand, type, category, price
+    public static Item getItemBy(String type, String value) throws Exception {
+        DBUtil dbUtils = new DBUtil();
+        Connection conn = dbUtils.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Item item = null;
+
+        try {
+            if (type.equals("name")) {
+                ps = conn.prepareStatement("SELECT * FROM \"Data\" WHERE \"name\"=?");
+            } else if (type.equals("brand")){
+                ps = conn.prepareStatement("SELECT * FROM \"Data\" WHERE \"brand\"=?");
+            } else if (type.equals("type")){
+                ps = conn.prepareStatement("SELECT * FROM \"Data\" WHERE \"type\"=?");
+            } else if (type.equals("category")){
+                ps = conn.prepareStatement("SELECT * FROM \"Data\" WHERE \"category\"=?");
+            } else{
+                ps = conn.prepareStatement("SELECT * FROM \"Data\" WHERE \"price\"=?");
+            }
+
+            ps.setString(1, value);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                item = new Item();
+                item.setName(rs.getString("name"));
+                item.setBrand(rs.getString("brand"));
+                item.setAType(rs.getString("type"));
+                item.setCategory(rs.getString("category"));
+                item.setPrice(rs.getString("price"));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            dbUtils.close();
+        }
+
+        return item;
+
+    }
+
+
+
+}
--- app/controllers/Item.java
+++ app/controllers/Item.java
@@ -1,27 +1,59 @@
 package controllers;
 
-
-
 public class Item {
-	private String picture;
-	private String name;
-	
-	public Item(String picture, String name) {
-		this.picture = picture;
-		this.name = name;
-	}
-	public String getPicture() {
-		return picture;
-	}
-	public void setPicture(String picture) {
-		this.picture = picture;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-		
-	
+    public String itemName;
+    public String itemBrand;
+    public String itemType;
+    public String itemCategory;
+    public String itemPrice;
+    public String picture;
+
+
+    public String getName() {
+        return this.itemName;
+    }
+
+    public void setName(String itemN) {
+        this.itemName = itemN;
+    }
+
+    public String getBrand() {
+        return itemBrand;
+    }
+
+    public void setBrand(String itemB) {
+        this.itemBrand = itemB;
+    }
+
+    public String getType() {
+        return itemType;
+    }
+
+    public void setAType(String itemT) {
+        this.itemType = itemT;
+    }
+
+    public String getCategory() {
+        return itemCategory;
+    }
+
+    public void setCategory(String itemC) {
+        this.itemCategory = itemC;
+    }
+
+    public String getPrice() {
+        return itemPrice;
+    }
+
+    public void setPrice(String itemP) {
+        this.itemPrice = itemP;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
 }
