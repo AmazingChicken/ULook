a97203e84c1f98b0a07e435a0b72719c72a1fb9d
felixwaang <mr_totally_awesome@hotmail.com>
2017-10-19 02:12:08 +1100

    need to merge to pull

--- target/scala-2.12/routes/main/controllers/ReverseRoutes.scala
+++ target/scala-2.12/routes/main/controllers/ReverseRoutes.scala
@@ -1,12 +1,7 @@
 
 // @GENERATOR:play-routes-compiler
-<<<<<<< HEAD
 // @SOURCE:C:/Users/mr_to/ULook/conf/routes
-// @DATE:Tue Oct 17 21:13:27 AEDT 2017
-=======
-// @SOURCE:C:/Users/Daniel/ULook/conf/routes
-// @DATE:Wed Oct 18 20:25:14 AEDT 2017
->>>>>>> 0658d712bff933720594ea339531680f87773bfe
+// @DATE:Wed Oct 18 21:01:41 AEDT 2017
 
 import play.api.mvc.Call
 
